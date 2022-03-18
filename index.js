3c17ff3e5518646d1fd76c0be0d804c50bd5f62a
polo <paulguzdariviere@gmail.com>
2022-03-18 18:58:19 +0100

    Config: git, npm, webpack

---
+++ index.js
@@ -0,0 +1,1 @@
+import inputRaw from './src/inputRaw'
---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const RemoveEmptyScriptsPlugin = require("webpack-remove-empty-scripts")
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+// Utils
+
+const rootDir = process.cwd()
+
+// Config
+
+module.exports = (env, opts) => {
+  return {
+    mode: 'development',
+    entry: './index.js',
+    output: {
+      library: 'inputRaw',
+      libraryTarget: 'umd',
+      libraryExport: 'default',
+      path: path.join(rootDir, '/dist/'),
+      // publicPath: '/',
+      filename: 'inputRaw.js',
+    },
+    module: {
+      rules: [
+        // Pkg-related rules (internal)
+        {
+          test: /\.js$/i,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
+                plugins: ['@babel/plugin-proposal-class-properties'],
+              }
+            },
+          ]
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                sassOptions: {
+                  indentedSyntax: true,
+                  indentWidth: 2,
+                  includePaths: [
+                    path.join(rootDir, '/app/ui/assets/sass'),
+                  ],
+                },
+              },
+            },
+          ]
+        },
+      ]
+    },
+    resolve: {
+      alias: {
+        '@': path.join(rootDir, '/src/'),
+      }
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'inputRaw.css',
+      }),
+      new RemoveEmptyScriptsPlugin(),
+      // new BundleAnalyzerPlugin({
+      //   analyzerMode: 'server',
+      //   analyzerPort: 9001,
+      //   generateStatsFile: true,
+      //   openAnalyzer: false,
+      // }),
+    ],
+  }
+}
