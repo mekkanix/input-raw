fe801b126fd642c75133cbaca3613b2ee3f2dbee
polo <paulguzdariviere@gmail.com>
2022-03-24 19:05:30 +0100

    Modular implementation of actions buttons

--- src/js/PropToolbar.js
+++ src/js/PropToolbar.js
@@ -16,73 +16,19 @@ export default class PropToolbar {
   }
 
   constructor() {
-    this.computeActions()
-    this._generateDOM()
+    this._initActions()
+    this._initDOM()
+    this._updateDOMFromActions()
   }
 
-  computeActions() {
-    const state = this.state
-    const actions = [
-      {
-        name: 'edit',
-        enabled: state.initialized && state.editable && !state.editing,
-        class: 'edit',
-        handler: this.onEditClick,
-        icon: 'edit',
-      },
-      {
-        name: 'validate_edit',
-        enabled: state.editable && state.editing && !state.errored,
-        class: 'validate-edit',
-        handler: this.onValidateEditClick,
-        icon: 'check',
-      },
-      {
-        name: 'cancel_edit',
-        enabled: state.editable && state.editing,
-        class: 'cancel-edit',
-        handler: this.onCancelEditClick,
-        icon: 'times',
-      },
-      {
-        name: 'delete',
-        enabled: state.initialized,
-        class: 'delete',
-        handler: this.onDeleteClick,
-        icon: 'trash',
-      },
-      {
-        name: 'convert_to_object',
-        enabled: state.initialized && state.toObject,
-        class: 'convert2object',
-        handler: this.onConvertToObjectClick,
-        text: '{}',
-      },
-      {
-        name: 'convert_to_array',
-        enabled: state.initialized && state.toArray,
-        class: 'convert2array',
-        handler: this.onConvertToArrayClick,
-        text: '[]',
-      },
-    ]
-    this._actions = actions
-  }
-
-  updateState(prop, value) {
-    if (this.state.hasOwnProperty(prop)) {
-      this.state[prop] = value
-    }
-    this.computeActions()
-    this._generateDOM()
-  }
-
-  _generateDOM() {
+  _initDOM() {
+    console.log(this._actions);
+    // Base
     this.attachedElement.classList.add('ir__prop-toolbar')
-    // Actions management
+    // Actions
     const actionsElement = document.createElement('div')
     actionsElement.classList.add('ir__prop-toolbar__actions')
-    for (const action of this._actions) {
+    this._actions.map(action => {
       const actionElement = document.createElement('div')
       actionElement.classList.add('ir__prop-toolbar__action')
       actionElement.classList.add(action.class)
@@ -95,10 +41,73 @@ export default class PropToolbar {
         icnHTML = action.text
       }
       actionElement.innerHTML = icnHTML
+      action.attachedElement = actionElement
       actionsElement.appendChild(actionElement)
-    }
-    //
+    })
     this.attachedElement.appendChild(actionsElement)
+  }
+
+  _initActions() {
+    // Note: `s` stands for `state` in following callbacks.
+    const actions = [
+      {
+        name: 'edit',
+        enabled: s => s.initialized && s.editable && !s.editing,
+        class: 'edit',
+        handler: this.onEditClick,
+        icon: 'edit',
+      },
+      {
+        name: 'validate_edit',
+        enabled: s => s.editable && s.editing && !s.errored,
+        class: 'validate-edit',
+        handler: this.onValidateEditClick,
+        icon: 'check',
+      },
+      {
+        name: 'cancel_edit',
+        enabled: s => s.editable && s.editing,
+        class: 'cancel-edit',
+        handler: this.onCancelEditClick,
+        icon: 'times',
+      },
+      {
+        name: 'delete',
+        enabled: s => s.initialized,
+        class: 'delete',
+        handler: this.onDeleteClick,
+        icon: 'trash',
+      },
+      {
+        name: 'convert_to_object',
+        enabled: s => s.initialized && s.toObject,
+        class: 'convert2object',
+        handler: this.onConvertToObjectClick,
+        text: '{}',
+      },
+      {
+        name: 'convert_to_array',
+        enabled: s => s.initialized && s.toArray,
+        class: 'convert2array',
+        handler: this.onConvertToArrayClick,
+        text: '[]',
+      },
+    ]
+    this._actions = actions
+  }
+
+  _updateDOMFromActions() {
+    for (const action of this._actions) {
+      const enabled = action.enabled(this.state)
+      action.attachedElement.style.display = enabled ? 'block' : 'none'
+    }
+  }
+
+  updateState(prop, value) {
+    if (this.state.hasOwnProperty(prop)) {
+      this.state[prop] = value
+    }
+    this._updateDOMFromActions()
   }
 
   onEditClick(e) {
@@ -106,22 +115,22 @@ export default class PropToolbar {
   }
 
   onValidateEditClick(e) {
-
+    console.log(e);
   }
 
   onCancelEditClick(e) {
-
+    console.log(e);
   }
 
   onDeleteClick(e) {
-
+    console.log(e);
   }
 
   onConvertToObjectClick(e) {
-
+    console.log(e);
   }
 
   onConvertToArrayClick(e) {
-
+    console.log(e);
   }
 }
